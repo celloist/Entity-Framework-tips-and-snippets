6218620f62eb55c870f2ac2de27bde7ef7507ab5
Alhric Lacle <celloist@users.noreply.github.com>
2020-12-04 09:52:27 +0100

    Update Snippets.cs

--- Snippets.cs
+++ Snippets.cs
@@ -1,1 +1,8 @@
 
+: 
+             modelBuilder.Entity<Document>()
+            .OwnsOne(e => e.DocumentPropeties)
+            .Property(p => p.Languages)
+            .HasConversion(
+                v => string.Join(",", v),
+                v => v.Split(',', StringSplitOptions.RemoveEmptyEntries).ToList());
